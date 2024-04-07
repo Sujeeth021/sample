ac65137c01bc8e9c6aa1ce37db10c3ad0b344626
Sujeeth021 <sahasujee@gmail.com>
2024-04-07 15:33:53 +0530

    Second Commit

--- prime.py
+++ prime.py
@@ -2,8 +2,8 @@ a=567
 b=0
 for i in range(2,a//2):
     if(a%i==0):
-        print("Not a Prime")
+        print(i," is not a Prime")
         b=1
         break
 if b==0:
-    print("Prime")
+    print(i,"is Prime")
